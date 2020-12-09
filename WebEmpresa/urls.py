1f57be3fe2d7fe413917fed59e518f51210ff0cc
Jorge Cabral <jorgect99@gmail.com>
2020-12-09 15:22:43 -0700

    Fusion Frontend y Backend

--- WebEmpresa/urls.py
+++ WebEmpresa/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
+    #paths core
+    path('', include('core.urls')),
+    #paths admin
     path('admin/', admin.site.urls),
+
 ]
