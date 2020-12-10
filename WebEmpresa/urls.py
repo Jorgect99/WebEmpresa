732211ef43a7f8ecfd2c1e9bcbc2ada0954a6afd
Jorge Cabral <jorgect99@gmail.com>
2020-12-09 18:59:58 -0700

    App Services

--- WebEmpresa/urls.py
+++ WebEmpresa/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
 
 urlpatterns = [
     #paths core
     path('', include('core.urls')),
+    #paths services
+    path('', include('services.urls')),
     #paths admin
     path('admin/', admin.site.urls),
 
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
