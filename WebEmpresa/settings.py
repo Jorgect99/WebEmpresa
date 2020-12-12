96d7e8c9a8202a8f16ed823d8266fc61f1412b5d
Jorge Cabral <jorgect99@gmail.com>
2020-12-12 12:52:09 -0700

    Ckeditor 'what you see is what you get'

--- WebEmpresa/settings.py
+++ WebEmpresa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'ckeditor',
     'core',
     'pages.apps.PagesConfig',
     'social.apps.SocialConfig',
@@ -129,3 +130,15 @@ STATIC_URL = '/static/'
 #media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#ckeditor
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+        ]
+    }
+}
